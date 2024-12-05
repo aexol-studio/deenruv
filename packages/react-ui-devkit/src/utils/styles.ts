b4f202dcbe1ad1d2b6bc9c12f6c3f1b73cd84818
Adam Michałowski <adam.michalowski@aexol.com>
2024-12-05 00:12:15 +0000

    fixed nav link hover in dark mode

--- packages/react-ui-devkit/src/utils/styles.ts
+++ packages/react-ui-devkit/src/utils/styles.ts
@@ -16,7 +16,7 @@ export const buttonVariants = cva(
                     'bg-stone-100 text-stone-900 hover:bg-stone-100/80 dark:bg-stone-800 dark:text-stone-50 dark:hover:bg-stone-800/80',
                 ghost: 'hover:bg-stone-100 hover:text-stone-900 dark:hover:bg-stone-800 dark:hover:text-stone-50',
                 'navigation-link':
-                    'text-navigation-link opacity-50 dark:opacity-70 hover:bg-navigation-link/20 hover:opacity-100 hover:text-navigation-link',
+                    'text-navigation-link opacity-50 dark:opacity-70 hover:bg-navigation-link/20 dark:hover:opacity-100 hover:opacity-100 dark:hover:bg-navigation-link/20 hover:text-navigation-link',
                 link: 'text-stone-900 underline-offset-4 hover:underline dark:text-stone-50',
             },
             size: {
