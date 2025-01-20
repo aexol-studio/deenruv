368c47481c96f2af0beca3e443f17a4c22de67d5
Artur Szarkowicz <artur.szarkowicz@aexol.com>
2025-01-20 12:00:15 +0100

    Add devTranslate vite plugin

--- apps/panel/vite.config.ts
+++ apps/panel/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import devTranslatePlugin, { Languages } from '@aexol/vite-plugin-dev-translate';
 
 export default defineConfig({
   base: '/admin-ui',
   server: { port: 3001 },
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    devTranslatePlugin({
+      apiKey: '4404413a.b16aaa6',
+      folderName: 'en',
+      localeDir: '../../packages/admin-dashboard/src/locales',
+      lang: Languages.ENUS,
+    }),
+  ],
 });
