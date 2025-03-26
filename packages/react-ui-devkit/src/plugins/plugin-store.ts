a36319c58273bd7abe63304c428225043b094b61
AleksanderBondar <darkduble12@gmail.com>
2025-03-26 10:36:21 +0100

    v0.0.101

--- packages/react-ui-devkit/src/plugins/plugin-store.ts
+++ packages/react-ui-devkit/src/plugins/plugin-store.ts
@@ -5,7 +5,8 @@ import { defaultInputComponents } from './default-input-components.js';
 
 const pagePathPrefix = 'admin-ui/extensions';
 
-const getExtensionsPath = (pluginName: string, path: string) => [pagePathPrefix, pluginName, path].join('/');
+const getExtensionsPath = (pluginName: string, path: string) =>
+    [pagePathPrefix, pluginName.toLowerCase().replace(' ', '-'), path].join('/');
 
 type I18Next = {
     addResourceBundle: (lng: string, ns: string, trans: object) => void;
@@ -36,7 +37,7 @@ export class PluginStore {
             el =>
                 el.pages?.map(route => ({
                     ...route,
-                    path: getExtensionsPath(el.name.toLowerCase().replace(' ', '-'), route.path),
+                    path: getExtensionsPath(el.name, route.path),
                 })) || [],
         );
         this.pluginsNavigationDataField.links = plugins.flatMap(el => {
@@ -44,7 +45,7 @@ export class PluginStore {
             return el.navMenuLinks.map(linkEl => ({
                 ...linkEl,
                 labelId: `${el.translations?.ns}.${linkEl.labelId}`,
-                href: getExtensionsPath(el.name.toLowerCase().replace(' ', '-'), linkEl.href),
+                href: getExtensionsPath(el.name, linkEl.href),
             }));
         });
         this.pluginsNavigationDataField.groups = plugins
