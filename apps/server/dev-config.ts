6d1a074201d942ae61aa41851b2524bd7262aad1
AleksanderBondar <darkduble12@gmail.com>
2025-08-12 08:24:49 +0200

    back db conn

--- apps/server/dev-config.ts
+++ apps/server/dev-config.ts
@@ -276,9 +276,9 @@ export const devConfig: DeenruvConfig = {
     type: "postgres",
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USERNAME || "liogalaktyka-shop",
-    password: process.env.DB_PASSWORD || "liogalaktyka-shop",
-    database: process.env.DB_NAME || "liogalaktyka-shop",
+    username: process.env.DB_USERNAME || "deenruv",
+    password: process.env.DB_PASSWORD || "deenruv",
+    database: process.env.DB_NAME || "deenruv",
     schema: process.env.DB_SCHEMA || "public",
   },
   paymentOptions: {
